86e7b5706c68efc0dd97c557c788bdc0fb0965b2
Isabella <isabellayk13@gmail.com>
2026-08-26 16:55:27 -0300

    Feat: Fazendo seção Nossa Solução

--- src/js/script.js
+++ src/js/script.js
@@ -4,3 +4,19 @@ const navMenu = document.querySelector('.nav-menu');
 menuToggle.addEventListener('click', () => {
   navMenu.classList.toggle('active');
 });
+
+// SEÇÃO NOSSA SOLUÇÃO
+const carousel = document.querySelector('.solucao-carousel');
+const btnPrev = document.querySelector('.btn-prev');
+const btnNext = document.querySelector('.btn-next');
+
+
+const scrollAmount = 370;
+
+btnNext.addEventListener('click', () => {
+  carousel.scrollBy({ left: scrollAmount, behavior: 'smooth' });
+});
+
+btnPrev.addEventListener('click', () => {
+  carousel.scrollBy({ left: -scrollAmount, behavior: 'smooth' });
+});
